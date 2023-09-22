e774bb8eff1b652d87111b34bb9e3e27382ff301
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-22 18:41:04 -0300

    Feat: created global style and default colors theme

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+      font-size: 62.5%;
+  }
+
+  :focus {
+      outline: 0;
+      box-shadow: 0 0 0 2px ${(props) => props.theme['purple-500']};
+    }
+
+    body {
+      background-color: ${(props) => props.theme['gray-800']};
+      color: ${(props) => props.theme['gray-100']};
+      -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+      font: 400 1.6rem Roboto, sans-serif;
+    }
+`
