b2c3c0955e9161087b7b80feb9e4f58ba60e481f
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-23 13:37:41 -0300

    Feat: added new dependencies and new configs to app

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,11 @@ module.exports = {
   settings: {
     "react": {
       "version": "detect"
+    },
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
     }
   },
 
