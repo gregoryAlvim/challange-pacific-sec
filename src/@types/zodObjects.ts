0db55552fae619ade523b4fef006c8b9d8d63831
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-24 10:10:37 -0300

    Feat: created user objected zod and user type

---
+++ src/@types/zodObjects.ts
@@ -0,0 +1,35 @@
+import * as z from 'zod'
+
+const noScripts = (value: string) => {
+  const scriptRegex = /<script\b[^>]*>[\s\S]*<\/script>/i
+  return scriptRegex.test(value) ? null : value
+}
+
+export const UserSchema = z.object({
+  name: z
+    .string()
+    .nonempty('O nome é obrigatório')
+    .refine(noScripts, {
+      message: 'Este campo não permite scripts.',
+    })
+    .transform((name) => {
+      return name
+        .trim()
+        .split(' ')
+        .map((word) => {
+          return word[0].toLocaleUpperCase().concat(word.substring(1))
+        })
+        .join(' ')
+    }),
+  email: z
+    .string()
+    .nonempty('O e-mail é obrigatório')
+    .email('Formato de e-mail inválido')
+    .toLowerCase(),
+  dateOfBirth: z.string().nonempty('A data de nascimento é obrigatória'),
+  phone: z.string().nonempty('O telefone é obrigatório').refine(noScripts, {
+    message: 'Este campo não permite scripts.',
+  }),
+  linkedin: z.string().optional(),
+  github: z.string().optional(),
+})
